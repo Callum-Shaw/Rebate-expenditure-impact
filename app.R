ee4b6c3a7190dfc12c882a818c6179ab98a7c105
Callum-Shaw <53550119+Callum-Shaw@users.noreply.github.com>
2019-10-07 14:13:51 +1100

    Upload app & function

---
+++ app.R
@@ -0,0 +1,83 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(readxl)
+library(shinyWidgets)
+
+source('function.R')
+
+
+grattan_slider <- function(variable, title, subtitle, default = 0){
+    
+    sliderInput(inputId = variable, 
+                
+                label = HTML(paste0(tags$span(style = "color:#6A737B", title),
+                                    
+                                    tags$br(),
+                                    
+                                    tags$span(style = "font-weight:normal; color:#6A737B; font-size:12px", subtitle))),
+                
+                min = 0, max = 45,
+                
+                value = default, step = 1)
+    
+}
+
+
+grattan_check <- function(variable, title, value){
+    
+    checkboxInput(inputId = variable, 
+                label = HTML(paste0(tags$span(style = "font-weight:normal;color:#6A737B", title))),
+                value = value)
+    
+}
+
+ui <- fluidPage(
+    chooseSliderSkin("Flat", color = "#F68B33"),
+    # Application title
+    title = "Rebate expenditure",
+    # Sidebar with a slider input for number of bins 
+    hr(),
+    
+    titlePanel(
+                    span("What is the impact of changes in the PHI rebate on private contributions?",
+                         
+                         style = "color:#6A737B; font-weight:bold; font-size:24px")),
+    hr(),
+    
+    sidebarLayout(
+        sidebarPanel(grattan_slider(var = "q1",
+                       
+                       title = "What proportion of the population will have private health insurance without the private health insurance rebate?",
+                       
+                       subtitle = "% of population")
+        ,
+        grattan_check("q2",
+                    "Include Government expenditure on the rebate on general insurance (extras) in total rebate expenditure, in addition to expenditure on the private hospital rebate",
+                    value = FALSE),
+        grattan_check("q3",
+                      "Include extras expenditure in the revenue generated",
+                      value = FALSE),
+        grattan_check("q4",
+                      "Include out-of-pocket costs associated with public and private hospital care in the contributions generated from the rebate",
+                      value = FALSE))
+    ,mainPanel(textOutput("txtOutput")))
+
+)
+
+server <- function(input, output) {
+    output$txtOutput = renderText({
+        paste0("Every dollar of rebate expenditure generates an extra $",round(estimated_impact(input$q1,input$q2,input$q3,input$q4),4)," of private contributions for healthcare.")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ function.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+
+### Data cleaning
+aihw_hwe_77_data_tables <- read_excel("C:/Users/CSSHAW/Dropbox (Grattan Institute)/Pub Pte efficiency/Shiny App/Data/aihw-hwe-77-data-tables.xls", 
+                                      sheet = "Table A3", range = "A4:N25", 
+                                      na = c(". .","—")) 
+aihw_hwe_77_data_tables[aihw_hwe_77_data_tables == "—"] <- 0
+data <- aihw_hwe_77_data_tables %>%
+  mutate(Area = `Area of expenditure`,
+         Rebate = as.numeric(aihw_hwe_77_data_tables$`Premium rebates`),
+         Insurance = as.numeric(aihw_hwe_77_data_tables$`Health insurance providers`),
+         Individuals = as.numeric(aihw_hwe_77_data_tables$Individuals)) %>%
+  select(Area,Rebate,Insurance,Individuals,Total = Total...5)
+
+estimated_impact <- function(i1,i2,i3,i4){
+
+q1 = i1
+q2 = ifelse(i2 == FALSE,TRUE,FALSE) 
+q3 = ifelse(i3 == FALSE,TRUE,FALSE)
+q4 = i4
+
+### Calculation
+
+## Get admin spend for Total, Insurance & Rebate
+admin <- data %>%
+  filter(Area == "Administration")
+
+## Get out of pocket for individual 
+OOP_spend <- data %>%
+  filter(Area %in% c("Hospitals"))  %>%
+  select(Individuals) %>%
+  group_by() %>%
+  summarise(sum = sum(Individuals))
+
+## Get extras spend for Insurance & Rebate
+extras_spend <- data %>%
+  filter(Area %in% c("Primary health care","Other services")) %>%
+  group_by() %>%
+  summarise(rebate_extras = sum(Rebate),
+            insurance_extras = sum(Insurance))
+
+# Get total spend for all
+total_spend <- data %>%
+  filter(Area == "Total health expenditure")
+
+Rebate    <- total_spend$Rebate - as.numeric(q2)*extras_spend$rebate_extras 
+Insurance <- (total_spend$Insurance - 
+                as.numeric(q3)*extras_spend$insurance_extras + 
+                as.numeric(q4)*OOP_spend -
+                (admin$Insurance/total_spend$Insurance - admin$Total/total_spend$Total) * total_spend$Insurance) 
+answer    <- ((45-q1)/45)*(Insurance/Rebate)
+return(answer$sum)
+}
+
+(admin$Insurance/total_spend$Insurance - admin$Total/total_spend$Total) * total_spend$Insurance
+
