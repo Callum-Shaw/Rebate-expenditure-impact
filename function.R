53c11bf496c4d30bc6a72439023ac9b4e2eca367
Callum Shaw <callum.shaw@grattaninstitute.edu.au>
2019-10-07 16:19:29 +1100

    function changed

--- function.R
+++ function.R
@@ -49,7 +49,7 @@ Rebate    <- total_spend$Rebate - as.numeric(q2)*extras_spend$rebate_extras
 Insurance <- (total_spend$Insurance - 
                 as.numeric(q3)*extras_spend$insurance_extras + 
                 as.numeric(q4)*OOP_spend -
-                (admin$Insurance/total_spend$Insurance - admin$Total/total_spend$Total) * admin$Insurance) 
+                (admin$Insurance/total_spend$Insurance - admin$Total/total_spend$Total) * total_spend$Insurance) 
 answer    <- ((45-q1)/45)*(Insurance/Rebate)
 return(answer$sum)
 }
